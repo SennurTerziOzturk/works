d07849291f7c03c0d0a2a6c0b4b556322e42c0f0
SennurTerziOzturk <78926408+SennurTerziOzturk@users.noreply.github.com>
2021-02-11 21:17:46 +0300

    1

---
+++ 1-Start/1-Start/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  1-Start
+//
+//  Created by Şennur Öztürk on 29.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func FncSendData(_ sender: UIButton) {
+        print("send")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    
+    }
+
+
+}
+
